c26350121609f8e29664c05480dfc8d510c66ea5
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-09 15:57:37 -0700

    finished

--- farm-animals/farm-animals.js
+++ farm-animals/farm-animals.js
@@ -1,16 +1,20 @@
 
-import { farmAnimalLi, findById } from '../utils.js';
+import { findById } from '../utils.js';
 import { animals } from '../data.js';
 
 
-
+//console.log(window.location.search);
 const params = new URLSearchParams(window.location.search);
 
-const animals = findById(params.get('id'), animals);
+const animal = findById(params.get('id'), animals);
 
-
+//console.log('animal-name');
 const animalName = document.getElementById('animal-name');
-animalName.textContent = farmAnimal.name;
+const img = document.getElementById('img');
+const animalQuote = document.getElementById('animal-quote');
 
-const div = 
+
+animalName.textContent = animal.name;
+animalQuote.textContent = animal.says;
+img.src = `../assets/${animal.type}.svg`;
 
